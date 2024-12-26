3d0dc70b3ff40e77fbe941d576bcc29062beaf20
Maksim Gruzdev <mgruzdev@cheby.ru>
2024-12-26 23:50:20 +0300

    #120 emulate user input

--- fuzz/target.js
+++ fuzz/target.js
@@ -6,9 +6,12 @@ const rp_valid = [
 
 const fuzz = async function (buf) {
 	const s = buf.toString('utf8')
-	const splitIndex = 10
+	const splitIndex = 7
 	const branch = s.slice(0, splitIndex)
-	const release = s.slice(splitIndex)
+	const release = s.slice(splitIndex, splitIndex * 2)
+	const fuzz_load = s.slice(splitIndex * 2)
+	global.FUZZ_LOAD_ASK = fuzz_load
+	global.FUZZ_LOAD = fuzz_load
 	try {
 		await main ([branch, 'to', release])
 	} catch (x) {
