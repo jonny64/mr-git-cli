74b55bcfdc57bba65417d226b6a9d824646b282a
Maksim Gruzdev <mgruzdev@cheby.ru>
2024-12-26 21:40:18 +0300

    #120 fuzz cov RE

--- fuzz/target.js
+++ fuzz/target.js
@@ -6,15 +6,11 @@ const rp_valid = [
 
 const fuzz = async function (buf) {
 	const s = buf.toString('utf8')
-	const splitIndex = Math.floor(s.length / 2)
+	const splitIndex = 10
 	const branch = s.slice(0, splitIndex)
 	const release = s.slice(splitIndex)
 	try {
 		await main ([branch, 'to', release])
-		await main ([branch, 'from', release])
-		await main ([branch])
-		await main ([release])
-		await main ([s])
 	} catch (x) {
 		for (let i of rp_valid) {
 			if (x.message.indexOf(i) !== -1) return
