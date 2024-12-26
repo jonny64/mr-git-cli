ca51ea6f5009398b40f570cf8fee715eb6dc968c
Maksim Gruzdev <mgruzdev@cheby.ru>
2024-12-26 22:20:03 +0300

    #120 ShellCommand cov++

--- tests/ShellCommand.js
+++ tests/ShellCommand.js
@@ -9,7 +9,16 @@ describe('random input', () => {
 			await new ShellCommand ({cmd: 'exit 1'}).run ()
 		})
 	})
+
 	it ('echo 1', async (t) => {
 		assert.strictEqual(await (new ShellCommand ({cmd: 'echo 1'}).run ()), '1')
+	})
+
+	it ('git status fuzz', async (t) => {
+		assert.strictEqual(await (new ShellCommand ({cmd: 'git status', isFuzz: true}).run ()), '')
+	})
+
+	it ('git status silent fuzz', async (t) => {
+		assert.strictEqual(await (new ShellCommand ({cmd: 'git status', isFuzz: true}).runSilent ()), '')
 	})
 })
