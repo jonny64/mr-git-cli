e69bebc81e47ab3465f537b2895b0920544f672d
Maksim Gruzdev <mgruzdev@cheby.ru>
2025-01-27 00:12:07 +0300

    #113 print mr.test output as it goes

--- tests/ShellCommand.js
+++ tests/ShellCommand.js
@@ -4,6 +4,34 @@ const ShellCommand = require ('../lib/ShellCommand')
 
 
 describe('random input', () => {
+
+	const makeSpawnMock = (o = {}) => {
+
+		const {stdout, stderr, close} = o
+
+		return (command, args, options) => ({
+			stdout: {
+				on: (event, callback) => {
+					if (event === 'data') {
+						return callback(`${stdout}\n`)
+					}
+				},
+			},
+			stderr: {
+				on: (event, callback) => {
+					if (event === 'data') {
+						return callback(`${stderr}\n`)
+					}
+				},
+			},
+			on: (event, callback) => {
+				if (event === 'exit') {
+					return callback(o.exit)
+				}
+			},
+		})
+	}
+
 	it ('exit 1', async (t) => {
 		assert.rejects(async () => {
 			await ShellCommand.withText ('exit 1').run ()
@@ -15,13 +43,13 @@ describe('random input', () => {
 	})
 
 	it ('exec ok', async (t) => {
-		const exec = (cmd, o, callback) => callback ('', '', '')
-		assert.strictEqual(await (new ShellCommand ({cmd: 'git fetch', exec}).runSilent ()), '')
+		const spawn = makeSpawnMock ({stdout: '', stderr: '', exit: 0})
+		assert.strictEqual(await (new ShellCommand ({cmd: 'git fetch', spawn}).runSilent ()), '')
 	})
 
 	it ('exec fail', async (t) => {
-		const exec = (cmd, o, callback) => callback (new Error ('Command failed: '), 'not a repo', '')
-		assert.rejects(new ShellCommand ({cmd: 'git fetch', exec}).runSilent (), {message: 'not a repo'})
+		const spawn = makeSpawnMock ({stdout: '', stderr: 'not a repo', exit: -127})
+		assert.rejects(new ShellCommand ({cmd: 'git fetch', spawn}).runSilent (), {message: 'not a repo'})
 	})
 
 	it ('git status fuzz', async (t) => {
